a27abd86d69f8e0e683613a6a53305485a8d478e
Aerialace <areialace@gmail.com>
2017-12-23 21:41:33 +1100

    Initial

---
+++ ItsAlive/ItsAlive/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ItsAlive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "192.168.1.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "192.168.1.254";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(246, 79);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "?";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(571, 245);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "?";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(360, 317);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 44);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(405, 245);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 25);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "?";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(86, 325);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 25);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "0";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "It\'s Alive";
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(26, 325);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 25);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Pings";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(26, 138);
+            this.label9.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(126, 25);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "192.168.1.4";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(246, 138);
+            this.label10.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(24, 25);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "?";
+            // 
+            // label11
+            // 
+            this.label11.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(756, 23);
+            this.label11.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 25);
+            this.label11.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 384);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "It\'s Alive";
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ ItsAlive/ItsAlive/Form1.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Mail;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows.Forms;
+using static System.Text.UTF8Encoding;
+using static ItsAlive.Properties.Resources;
+
+namespace ItsAlive
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker _pingBw = new BackgroundWorker(); //Ping background worker!!
+        private BackgroundWorker _emailBw = new BackgroundWorker(); //Email Background Worker
+
+        private bool _router = false; //Router Status bool
+        private bool _nas = false; // server status bool
+        private bool _mailSent = false; //Email status bool
+        private int _pings = 0; // num of pings 
+        private int _downs = 0; //Counts number of down pings
+        private bool _server;
+        private bool _running = false;
+        private bool _internet = false;
+        private bool _main = false;
+        private bool _vpn = false; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Config background worker for the pings
+            _pingBw.WorkerReportsProgress = true;
+            _pingBw.WorkerSupportsCancellation = true;
+            _pingBw.DoWork += new DoWorkEventHandler(ping_bw_DoWork);
+            _pingBw.ProgressChanged += new ProgressChangedEventHandler(ping_bw_ProgressChanged);
+            _pingBw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(ping_bw_RunWorkerCompleted);
+            //Config background worker for email
+            _emailBw.WorkerReportsProgress = true;
+            _emailBw.WorkerSupportsCancellation = false;
+            _emailBw.DoWork += new DoWorkEventHandler(email_bw_DoWork);
+            _emailBw.ProgressChanged += new ProgressChangedEventHandler(email_bw_ProgressChanged);
+            _emailBw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(email_bw_RunWorkerCompleted);
+        }
+
+        //Minimize to system tray instead of taskbar
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon1.Visible = true;
+                Hide();
+            }
+        }
+
+        //Click system tary icon to maximize
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        //Pings the target ip and returns the reply
+        private PingReply PingIp(string ip)
+        {
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+            pingSender.PingCompleted += new PingCompletedEventHandler(PingCompletedCallback);
+            options.DontFragment = true;
+            const string data = "a";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            const int timeout = 10;
+            try
+            {
+                PingReply reply = pingSender.Send(ip, timeout, buffer, options);
+                return reply;
+            }
+            catch (PingException e)
+            {
+                Invoke(new Action(() => { label7.Text = "Ping error: " + e; }));
+                _pingBw.CancelAsync();
+                throw;
+            }
+        }
+
+        private void PingCompletedCallback(object sender, PingCompletedEventArgs e)
+        {
+            // If an error occurred, display the exception to the user. 
+            if (e.Error != null)
+            {
+                label6.Text = "Ping failed";
+            }
+        }
+
+        //Calls pingIP() and will check the returnd ping's Status and set a boolean value for each device
+        private void Pinger()
+        {
+            PingReply ping1;
+            PingReply ping2;
+            PingReply ping3;
+            PingReply ping4;
+            PingReply ping5;
+            if (!_vpn)
+            {
+                ping1 = PingIp(Thanatos);
+                ping2 = PingIp(Router);
+                ping3 = PingIp(Elyptor);
+                ping4 = PingIp(Google);
+                ping5 = PingIp(Helios);
+            }
+            else
+            {
+                ping1 = PingIp(Thanatos_VPN);
+                ping2 = PingIp(Router);
+                ping3 = PingIp(Elyptor_VPN);
+                ping4 = PingIp(Google);
+                ping5 = PingIp(Helios_VPN);
+            }
+            if (ping1.Status == IPStatus.Success)
+            {
+                _nas = true;
+            }
+            else
+            {
+                _nas = false;
+            }
+           
+            if (ping2.Status == IPStatus.Success)
+            {
+                _router = true;
+            }
+            else
+            {
+                _router = false;
+            }
+        
+            if (ping3.Status == IPStatus.Success)
+            {
+                _server = true;
+            }
+            else
+            {
+                _server = false;
+            }
+            
+            if (ping4.Status == IPStatus.Success)
+            {
+                _internet = true;
+            }
+            else
+            {
+                _internet = false;
+            }
+           
+            if (ping5.Status == IPStatus.Success)
+            {
+                _main = true;
+            }
+            else
+            {
+                _main = false;
+            }
+        }
+
+
+        //Start the ping background worker
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_pingBw.IsBusy != true && _running == false)
+            {
+                _router = false;
+                _nas = false;
+                label6.Text = "Started";
+                _running = true;
+                //button1.Enabled = false;
+                //button2.Enabled = true;
+                _pingBw.RunWorkerAsync();
+                button1.Text = "Stop";
+                return;
+            }
+
+            if (_pingBw.WorkerSupportsCancellation && _running)
+            {
+                //button1.Enabled = true;
+                //button2.Enabled = false;
+                _mailSent = false;
+                _pings = 0;
+                _pingBw.CancelAsync();
+                button1.Enabled = false;
+                label6.Text = "Stopping";
+                
+                while (_pingBw.CancellationPending == false)
+                {
+                   if (_pingBw.CancellationPending)
+                        break;
+                }
+                button1.Text = "Start";
+                label6.Text = "Stopped";
+                _running = false;
+                button1.Enabled = true;
+                
+
+            }
+        }
+
+
+        //Call Pinger(), report the progress to update the labels then sleep
+        private void ping_bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            while (true)
+            {
+                if (worker.CancellationPending )
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    Pinger();
+
+                    worker.ReportProgress(1);
+
+                    //System.Threading.Thread.Sleep(60000);
+                }
+            }
+        }
+
+        //Ping worker done, not that it should ever finish
+        private void ping_bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                label6.Text = "Stopped!";
+            }
+
+            else if (e.Error != null)
+            {
+                label6.Text = "Error: " + e.Error.Message;
+            }
+
+            else
+            {
+                label6.Text = "Done!";
+            }
+        }
+
+        //Used to update the labels for the status
+        private void ping_bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //If server is up and a email has been sent reset that an email has been sent,
+            //else if server is down router is up and has been down for 2 pings an email will be sent
+            if (_nas)
+            {
+                label3.Text = "Up";
+                _downs = 0;
+                if (_mailSent)
+                {
+                    _mailSent = false;
+                    label11.Text = "";
+                }
+            }
+            else
+            {
+                label3.Text = "Down";
+                _downs++;
+                if (_emailBw.IsBusy != true)
+                {
+                    if (_mailSent == false && _router && _downs == 2)
+                    {
+                        _emailBw.RunWorkerAsync();
+                        label5.Text = "starting";
+                    }
+                }
+            }
+
+            if (_server)
+            {
+                label10.Text = "Up";
+            }
+            else
+            {
+                label10.Text = "Down";
+            }
+
+            if (_router)
+            {
+                label4.Text = "Up";
+            }
+            else
+            {
+                label4.Text = "Down";
+                
+            }
+
+            if (_internet)
+            {
+                label5.Text = "Internet";
+            }
+            else
+            {
+                label5.Text = "No Internet";
+            }
+            if (_running)
+                _pings++;
+            label7.Text = _pings.ToString();
+        }
+
+        //Worker for Email
+        private void email_bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            Email mail = new Email();
+            if (_nas == false && _internet)
+            {
+                mail.SendEmail(Thanatos);
+                _mailSent = true;
+                worker.ReportProgress(1);
+            }
+            if (_server == false && _internet)
+            {
+                mail.SendEmail(Elyptor);
+                _mailSent = true;
+                worker.ReportProgress(1);
+            }
+        }
+
+        private void email_bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                label11.Text = ("Error: " + e.Error.Message);
+            }
+
+            else if (_mailSent)
+            {
+                label11.Text = "Sent!";
+            }
+        }
+
+        private void email_bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            label11.Text = "Sending";
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Email
+    {
+        
+        
+        public void SendEmail(String name)
+        {
+            SmtpClient client = new SmtpClient
+            {
+                Port = Convert.ToInt32(SMTP_PORT),
+                Host = SMTP_HOST,
+                EnableSsl = true,
+                Timeout = 10000,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new System.Net.NetworkCredential("areialace@gmail.com", "LOTRlotr123!@#")
+            };
+            MailMessage mm = new MailMessage("areialace@gmail.com", "areialace@gmail.com", name + " is down",
+                name + " is down");
+            mm.BodyEncoding = Encoding.UTF8;
+            mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+            client.Send(mm);
+        }
+    }
+}
